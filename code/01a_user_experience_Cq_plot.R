6758e46a32dcd48511a1c2ecc00a05a95d5a44ed
Jeffery <Nick.Jeffery@dfo-mpo.gc.ca>
2026-09-17 15:40:37 -0300

    updating and modifying user experience cq plot

--- code/01a_user_experience_Cq_plot.R
+++ code/01a_user_experience_Cq_plot.R
@@ -5,21 +5,27 @@
 library(dplyr)
 library(ggplot2)
 library(forcats)
+library(tidyr)
+library(stringr)
 
 # Load data ---------------------------------------------------------------
 
 user.cq <- read.csv("data/UserExperience_CqValues.csv", header = T) %>% glimpse()
 
 # fill in blanks
-user.cq$User.Experience[2:7] <- "No Lab Experience"
-user.cq$User.Experience[9:13] <- "Non-Molecular Lab Experience"
-user.cq$User.Experience[15:16] <- "Previous Molecular Lab Experience"
-user.cq$User.Experience[18:22] <- "Molecular Lab Experts"
+user.cq$User.Experience[2:9] <- "No Lab Experience"
+user.cq$User.Experience[10:17] <- "Non-Molecular Lab Experience"
+user.cq$User.Experience[18:21] <- "Previous Molecular Lab Experience"
+user.cq$User.Experience[22:29] <- "Molecular Lab Experts"
 
 #replace NAs with 0 - this won't affect Mean Cq values
 user.cq.fixed <- user.cq %>% 
                  mutate(across(where(is.numeric), ~replace_na(.,0)), 
                         Sample = paste0(Type, " ", Sample.ID))
+
+user.cq.fixed$Sample <- gsub(" AIS","", user.cq.fixed$Sample)
+user.cq.fixed$Sample <- gsub("Extraction Negative ","", user.cq.fixed$Sample)
+
 
 # Make the table long format to make ggplot happy
 
@@ -35,24 +41,28 @@ cq.long.fix <- cq.long %>%
 
 # Make some plots ---------------------------------------------------------
 
-ggplot(data=cq.long.fix, aes(x = Sample, y=value, fill = Field_Lab))+
-  geom_violin()+
-  theme_bw() # this looks terrible, not enough data
+# ggplot(data=cq.long.fix, aes(x = Sample, y=value, fill = Field_Lab))+
+#   geom_violin()+
+#   theme_bw() # this looks terrible, not enough data
 
-ggplot(data=cq.long.fix[-27,], aes(x=fct_inorder(Sample), y=value, fill=Field_Lab))+
-  stat_summary(fun = mean, geom = "point", size = 4, shape=21, colour = "black") +
+ggplot(data=cq.long.fix[-c(33,43:45,71:72,149),], 
+       aes(x=fct_inorder(Sample), y=value, fill=Field_Lab, shape=factor(Type)))+
+  stat_summary(fun = mean, geom = "point", size = 4) +
   stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.2) +
-  labs(fill="")+
+  scale_shape_manual(values=c(23,22, 21))+
+  scale_fill_manual(values = c("gray","black"))+
+  labs(fill="", shape="")+
   ylab(label = "Cq")+
   xlab(label="Sample")+
+  guides(fill = guide_legend(override.aes = list(shape = 21, size = 4)))+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 45,hjust = 1, vjust = 1),
-        text = element_text(size=14))
+        text = element_text(size=15))
 
-ggsave(filename = "UserExperience_Cq_comparison.png",
+ggsave(filename = "UserExperience_Cq_comparison.pdf",
        plot = last_plot(), 
        path = "figures/",
-       device = "png",
+       device = "pdf",
        width=10, height=8, dpi = 300)
 
 
